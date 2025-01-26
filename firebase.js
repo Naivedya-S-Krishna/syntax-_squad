b0c4efe0f1a15650bb0bd690d5bad5f5cab3da86
nandanaanil-28 <148792488+nandanaanil-28@users.noreply.github.com>
2025-01-26 07:58:03 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByy296XonpzXRIhIn7Wve1WkDBbzNpZP8",
+  authDomain: "skillswap-ab21f.firebaseapp.com",
+  projectId: "skillswap-ab21f",
+  storageBucket: "skillswap-ab21f.firebasestorage.app",
+  messagingSenderId: "846256020669",
+  appId: "1:846256020669:web:f3a9a86b0e48b80e1fabd4",
+  measurementId: "G-2WNV2PNHVL"
+};
